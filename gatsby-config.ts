37b12f5775723159701eaf5dfa53ad8543911720
Robert Parker <robo.parker@proton.me>
2024-07-29 11:17:26 -0500

    add google tag id

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -39,9 +39,7 @@ const config: GatsbyConfig = {
             options: {
                 // You can add multiple tracking ids and a pageview event will be fired for all of them.
                 trackingIds: [
-                    "GA-TRACKING_ID", // Google Analytics / GA
-                    "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-                    "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+                    "G-1VR9JFCXYP", // Google Analytics / GA
                 ],
                 // This object gets passed directly to the gtag config command
                 // This config will be shared across all trackingIds
