136585f839694a4e3f8f85db652ca991f548492f
roboparker <robo.parker@proton.me>
2024-08-27 21:18:54 -0700

    add twitter

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
 import * as React from "react"
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Github, Linkedin, EnvelopeAtFill } from 'react-bootstrap-icons';
+import { Github, Linkedin, EnvelopeAtFill, Twitter } from 'react-bootstrap-icons';
 import { StaticImage } from "gatsby-plugin-image";
 
 const NavigationBar: React.FC = () => {
     return (
         <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
             <Container>
-                <Navbar.Brand href="#home">
+                <Navbar.Brand href="/">
                     <StaticImage  className={"d-inline-block align-text-top me-2"} src="../images/favicon.png" alt="Logo for roboparker" width={24} height={24}/>
                     Roboparker
                 </Navbar.Brand>
@@ -15,6 +15,7 @@ const NavigationBar: React.FC = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
                         <Nav.Link href="https://www.linkedin.com/in/roboparker" target="_blank"><Linkedin className='mb-1' /> LinkedIn</Nav.Link>
+                        <Nav.Link href="https://x.com/robo_parker" target="_blank"><Twitter className='mb-1' /> Twitter</Nav.Link>
                         <Nav.Link href="https://github.com/roboparker" target="_blank"><Github className='mb-1' /> GitHub</Nav.Link>
                         <Nav.Link href="mailto:roboparker@proton.me"><EnvelopeAtFill className='mb-1' /> Email</Nav.Link>
                     </Nav>
