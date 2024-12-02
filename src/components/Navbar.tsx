a0a072c7a988dbfba10879931823778f21aad2cd
Robert Parker <robo.parker@proton.me>
2024-12-02 01:17:08 -0600

    Add twitch and bluesky

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Github, Linkedin, EnvelopeAtFill, Twitter } from 'react-bootstrap-icons';
+import { Github, Linkedin, EnvelopeAtFill, Twitter, Twitch, Cloud } from 'react-bootstrap-icons';
 import { StaticImage } from "gatsby-plugin-image";
 
 const NavigationBar: React.FC = () => {
@@ -15,7 +15,9 @@ const NavigationBar: React.FC = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
                         <Nav.Link href="https://www.linkedin.com/in/roboparker" target="_blank"><Linkedin className='mb-1' /> LinkedIn</Nav.Link>
+                        <Nav.Link href="https://bsky.app/profile/roboparker.bsky.social" target="_blank"><Cloud className='mb-1' /> BlueSky</Nav.Link>
                         <Nav.Link href="https://x.com/robo_parker" target="_blank"><Twitter className='mb-1' /> Twitter</Nav.Link>
+                        <Nav.Link href="https://www.twitch.tv/roboparker" target="_blank"><Twitch className='mb-1' /> Twitch</Nav.Link>
                         <Nav.Link href="https://github.com/roboparker" target="_blank"><Github className='mb-1' /> GitHub</Nav.Link>
                         <Nav.Link href="mailto:roboparker@proton.me"><EnvelopeAtFill className='mb-1' /> Email</Nav.Link>
                     </Nav>
